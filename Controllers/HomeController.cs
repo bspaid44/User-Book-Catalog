049d0f33c36de8700f3d6fd5d37c0b4d6be1dda7
bspaid44 <brandonspaid@gmail.com>
2024-04-01 09:08:50 -0400

    Added about section for project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace UserLibrary.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
