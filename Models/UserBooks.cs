de2b413c02d6605e22c67486ce1d1796fcfc3605
bspaid44 <brandonspaid@gmail.com>
2024-03-31 23:30:17 -0400

    Fixed adding multiple books to user list

--- Models/UserBooks.cs
+++ Models/UserBooks.cs
@@ -1,7 +1,10 @@
-﻿namespace UserLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserLibrary.Models
 {
     public class UserBooks
     {
+        public int Id { get; set; }
         public string UserBooksId { get; set; }
         public int BookId { get; set; }
         public Book Book { get; set; }
